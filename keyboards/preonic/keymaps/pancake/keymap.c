27f78a018ab0f8e409580cefba8cd9f30b568462
thirtypancakes <thirtypancakes@gmail.com>
2021-04-04 00:01:30 +0100

    added ctrl/quote mod tap, for ctrl on each side

--- keyboards/preonic/keymaps/pancake/keymap.c
+++ keyboards/preonic/keymaps/pancake/keymap.c
@@ -3,14 +3,15 @@
 
 // easier names for long keycodes
 enum my_keys {
-    WINLEFT = LCTL(LGUI(KC_LEFT)),
+    WINLEFT  = LCTL(LGUI(KC_LEFT)),
     WINRIGHT = RCTL(RGUI(KC_RGHT)),
-    WINUP = RGUI(KC_UP),
-    WINMIN = RGUI(KC_M),
-    CTRLESC = MT(MOD_RCTL, KC_ESC),
-    SHFTENT = RSFT_T(KC_ENT),
-    MVRGHT = LGUI(LALT(LSFT(KC_RGHT))),
-    MVLEFT = LGUI(LALT(LSFT(KC_LEFT)))
+    WINUP    = RGUI(KC_UP),
+    WINMIN   = RGUI(KC_M),
+    CTRLESC  = MT(MOD_RCTL, KC_ESC),
+    CQUOT    = MT(MOD_LCTL, KC_QUOT),
+    SHFTENT  = RSFT_T(KC_ENT),
+    MVRGHT   = LGUI(LALT(LSFT(KC_RGHT))),
+    MVLEFT   = LGUI(LALT(LSFT(KC_LEFT)))
 };
 
 // layers
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
     [_QWERTY] = LAYOUT_ortho_5x12(\
             KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_NO,    \
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_DEL,   \
-            CTRLESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
+            CTRLESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, CQUOT,    \
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, SHFTENT,  \
             KC_LCTL, _______, KC_LGUI, KC_LALT, TD(LLYR), KC_SPC, KC_BSPC, TT(_RAISE), KC_LALT, KC_RGUI, _______, _______),
 
